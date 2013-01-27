d3b64fd2b488f49409162141f25232cedbc97a51
Doc Walker <doc.walker@jameshardie.com>
2013-01-27 07:59:33 -0600

    Update copyright year

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: r_project
 # Recipe:: default
 #
-# Copyright 2012, James Hardie Building Products, Inc.
+# Copyright 2012-2013, James Hardie Building Products, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
