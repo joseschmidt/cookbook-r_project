a084849ab0a2c18cb19bb88139db843906da5cd4
Doc Walker <doc.walker@jameshardie.com>
2014-01-05 20:37:39 -0600

    Improve test coverage, cleanup descriptions

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -21,27 +21,43 @@ describe 'r_project::default' do
     end.converge(described_recipe)
   end # let
 
-  it 'should install R v0.0.0' do
+  it 'installs R v0.0.0' do
     expect(chef_run).to install_package('R').with(:version => '0.0.0')
   end # it
 
-  it 'should create remote_file owned by root:root' do
-    qcc_tar_gz = "qcc_2.718.tar.gz"
-    qcc_filename = "#{Chef::Config['file_cache_path']}/#{qcc_tar_gz}"
-    expect(chef_run).to create_remote_file(qcc_filename)
-      .with(:owner => 'root', :group => 'root')
-  end # it
+  context 'when qcc is installed' do
+    before do
+      # required for not_if attribute; prevents qcc library download
+      stub_command("echo 'library(qcc)' | R --vanilla --quiet")
+        .and_return(true)
+    end # before
 
-  it 'should install qcc library' do
-    qcc_tar_gz = "qcc_0.0.0.tar.gz"
-    qcc_filename = "#{Chef::Config['file_cache_path']}/#{qcc_tar_gz}"
-    code = "sudo -E sh -c 'R CMD INSTALL #{qcc_filename}'"
-    # chef_run.should execute_command code
-    pending 'TODO: figure out how to test not_if {}'
-  end # it
+    it 'does not create remote_file' do
+      qcc_tar_gz = "qcc_2.718.tar.gz"
+      qcc_filename = "#{Chef::Config['file_cache_path']}/#{qcc_tar_gz}"
+      expect(chef_run).to_not create_remote_file(qcc_filename)
+    end # it
 
-  it 'should not uninstall R' do
-    expect(chef_run).to_not run_execute("sudo -E sh -c 'R CMD REMOVE qcc'")
+    it 'does not install qcc library' do
+      expect(chef_run).to_not run_bash('install_qcc_library')
+    end # it
+  end # context
+
+  context 'when qcc is not installed' do
+    it 'creates remote_file owned by root:root' do
+      qcc_tar_gz = "qcc_2.718.tar.gz"
+      qcc_filename = "#{Chef::Config['file_cache_path']}/#{qcc_tar_gz}"
+      expect(chef_run).to create_remote_file(qcc_filename)
+        .with(:owner => 'root', :group => 'root')
+    end # it
+
+    it 'installs qcc library' do
+      expect(chef_run).to run_bash('install_qcc_library')
+    end # it
+  end # context
+
+  it 'does not uninstall R' do
+    expect(chef_run).to_not run_bash('uninstall_qcc_library')
   end # it
 
 end # describe
