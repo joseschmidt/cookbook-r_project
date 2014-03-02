30bd00764fa45f53f4e094eb160d046e7e80b955
Doc Walker <doc.walker@jameshardie.com>
2014-03-02 08:20:31 -0600

    Update metadata.rb and standardize layout

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,29 @@
 # encoding: utf-8
 name              'r_project'
-version           '0.2.0'
-
 maintainer        'James Hardie Building Products, Inc.'
 maintainer_email  'doc.walker@jameshardie.com'
+description       'Installs the R language, environment, and related packages'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license           'Apache 2.0'
-description       'Installs/Configures r_project'
+version           '0.2.0'
 
+#--------------------------------------------------------------------- recipes
+recipe            'r_project',
+                  'Installs the R language, environment, and related packages'
+
+#------------------------------------------------------- cookbook dependencies
 depends           'yum-epel'
+
+#--------------------------------------------------------- supported platforms
+# tested
+supports          'centos'
+
+# platform_family?('rhel'): not tested, but should work
+supports          'amazon'
+supports          'oracle'
+supports          'redhat'
+supports          'scientific'
+
+# platform_family?('debian'): not tested, but may work
+supports          'debian'
+supports          'ubuntu'
